21ac7f04ce3c3d4b1cfc63309ac5355f6da1850e
priscillavarrenti <pvarrenti@byupathway.edu>
2025-09-06 04:36:52 -0300

    intento

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
-const yearSpan = document.getElementById("currentyear");
-const currentyear= new Date().getFullYear();
-yearSpan.textContent = currentyear;
+
 
 const lastMod = document.getElementById("lastModified");
 lastMod.textContent = document.lastModified;
