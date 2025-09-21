a9fee350dc6b190b26da91a851631e73f673c92d
priscillavarrenti <pvarrenti@byupathway.edu>
2025-09-21 19:41:47 -0300

    done

--- week2/scripts/temples.js
+++ week2/scripts/temples.js
@@ -1,5 +1,5 @@
 const menuButton = document.getElementById("menu");
-const navigation = document.getElementById("nav-menu");
+const navMenu = document.getElementById("nav-menu");
 
 menuButton.addEventListener("click", () => {
     navMenu.classList.toggle("open");
