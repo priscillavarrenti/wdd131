88d9803df6a5218d3ba2cd4a781066a7d77f5d60
priscillavarrenti <pvarrenti@byupathway.edu>
2025-09-21 20:34:27 -0300

    first step

---
+++ week3/scripts/place.js
@@ -0,0 +1,18 @@
+function calculateWindChill(temp, windspeed) {
+    if (temp <= 50 && windspeed >3)
+        return Math.round(35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp * math-pow (windspeed, 0.16));
+    else { 
+        return 'n/a';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    const temperature= 50;
+    const windspeed= 10;
+
+    const windChill= calculateWindChill (temperature, windspeed);
+
+    const windchillElement= document.getElementById('windchill')
+    windchillElement.textContent= windChill;
+
+});
