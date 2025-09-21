bbcae7c2ada656be1a3c4b67f8d9dcfe673da4a6
priscillavarrenti <pvarrenti@byupathway.edu>
2025-09-21 19:12:13 -0300

    almost finiishj

--- week2/scripts/temples.js
+++ week2/scripts/temples.js
@@ -1,7 +1,7 @@
 const menuButton = document.getElementById("menu");
-const navigation = document.querySelector(".navigation");
+const navigation = document.getElementById("nav-menu");
 
 menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    menuButton.textContent = navigation.classList.contains("open") ? "close" : "menu";
+    navMenu.classList.toggle("open");
+    menuButton.textContent = navMenu.classList.contains("open") ? "✖" : "☰";
 });
