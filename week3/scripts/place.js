5eac9d65fce318a8bbc25fe401267daf32e41680
priscillavarrenti <pvarrenti@byupathway.edu>
2025-09-22 02:51:40 -0300

    new

--- week3/scripts/place.js
+++ week3/scripts/place.js
@@ -16,3 +16,10 @@ document.addEventListener('DOMContentLoaded', function(){
     windchillElement.textContent= windChill;
 
 });
+
+const yearSpan = document.getElementById("currentyear");
+const currentYear= new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastMod = document.getElementById("lastModified");
+lastMod.textContent = document.lastModified;
