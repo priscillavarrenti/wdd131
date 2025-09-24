490de127fa4acbfbcaaa4ef307759896924dd0d7
priscillavarrenti <pvarrenti@byupathway.edu>
2025-09-23 23:46:43 -0300

    js

--- week2/scripts/temples.js
+++ week2/scripts/temples.js
@@ -5,3 +5,10 @@ menuButton.addEventListener("click", () => {
     navMenu.classList.toggle("open");
     menuButton.textContent = navMenu.classList.contains("open") ? "✖" : "☰";
 });
+
+const yearSpan = document.getElementById("currentyear");
+const currentYear= new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastMod = document.getElementById("lastModified");
+lastMod.textContent = document.lastModified;
